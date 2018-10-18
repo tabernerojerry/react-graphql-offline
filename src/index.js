013146baeeb8ac354b7727a799f5184270c2343b
Tabernero Jerry <tabernerojerry@gmail.com>
2018-10-18 14:50:31 +0800

    Added Materialize CSS

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import { render } from "react-dom";
 
 import App from "./App.js";
 
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min.js";
+
 render(<App />, document.getElementById("root"));
